c8e3bd702650b818afc2ff627d01881877c3565d
rwn3developer <rwn3developer11@gmail.com>
2024-11-25 15:53:15 +0530

    delete & edit record

--- Express intro/index.js
+++ Express intro/index.js
@@ -20,29 +20,29 @@ let students = [
         lastname: 'Dhola',
         gender: 'male',
         email: 'ashish@test.in',
-        phone:1234567890
+        phone: 1234567890
     },
     {
         id: 103,
         firstname: "Palak",
         lastname: "Gohel",
-        email:"palak@test.in",
+        email: "palak@test.in",
         gender: 'female',
-        phone:4567890321
+        phone: 4567890321
     }
 ];
 
 
-app.get('/', (req, res)=>{
-    res.render('index',{students: students});
+app.get('/', (req, res) => {
+    res.render('index', { students: students });
 })
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
     res.render('about');
 })
 
-app.post('/addStudent', (req, res)=>{
+app.post('/addStudent', (req, res) => {
     // console.log(req.body);
-    const {id, firstname, lastname, email, gender, phone} = req.body;
+    const { id, firstname, lastname, email, gender, phone } = req.body;
     const newObj = {
         id: id,
         firstname: firstname,
@@ -54,13 +54,37 @@ app.post('/addStudent', (req, res)=>{
     students.push(newObj);
     console.log('New Student Added');
     return res.redirect("/");
+});
+
+app.get('/deleteStudent/:id', (req, res) => {
+    // console.log(req.params.id);
+    const id = req.params.id;
+    // filter, splice
+    students = students.filter((student) => {
+        return student.id != id
+    })
+    console.log('Record Delete')
+    return res.redirect('/');
+});
+
+app.get('/editStudent/:id', (req, res)=>{
+    const id = req.params.id;
+    const studentRecord = students.find((student)=>{
+        return student.id == id;
+    });
+    return res.render('editStudent', {student: studentRecord})
+    // console.log(studentRecord);
+});
+
+app.post('/editStudent', (req, res)=>{
+    console.log(req.body);
 })
 
 
-app.listen(port, (err)=>{
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log('Server not start');
-    }else{
+    } else {
         console.log(`Server start at http://localhost:${port}`);
     }
 });
