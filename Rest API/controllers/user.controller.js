e984e435896ea230b3d4e9def2eef5d3a483a220
Girish <rw3.girish.gk@gmail.com>
2025-01-30 16:30:22 +0530

    verifyToken

--- Rest API/controllers/user.controller.js
+++ Rest API/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 exports.addNewRegister = async (req, res) => {
   try {
@@ -13,6 +14,7 @@ exports.addNewRegister = async (req, res) => {
     let hashPassword = await bcrypt.hash(req.body.password, 10);
     // console.log(hashPassword);
     user = await User.create({ ...req.body, password: hashPassword });
+
     return res.status(201).json({ message: "User Register Success" });
   } catch (error) {
     console.log(error);
@@ -29,9 +31,12 @@ exports.loginUSer = async (req, res) => {
         return res.status(404).json({message: "User not Found"});
     }
     let chackedPass = await bcrypt.compare(req.body.password, user.password)
-    console.log(chackedPass);
+    // console.log(chackedPass);
     if(chackedPass){
-        return res.status(200).json({message: "Login Success", data: user});
+      let token = jwt.sign({
+        userId : user._id
+      }, 'red&white');
+        return res.status(200).json({message: "Login Success",token });
     }else{
         return res.status(400).json({message: "Password is Not Matched!!"})
     }
@@ -40,3 +45,8 @@ exports.loginUSer = async (req, res) => {
     return res.status(500).json({ message: "Server Error" });
   }
 };
+
+
+exports.profile = (req, res) => {
+  return res.json(req.user);
+}
--- Rest API/routes/user.routes.js
+++ Rest API/routes/user.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { addNewRegister, loginUSer } = require("../controllers/user.controller");
+const { addNewRegister, loginUSer, profile } = require("../controllers/user.controller");
+const { verifyToken } = require("../middleware/verifyToken");
 const routes = express.Router();
 
 routes.post("/register", addNewRegister);
 
-routes.post("/login", loginUSer)
+routes.post("/login", loginUSer);
+
+routes.get("/profile", verifyToken, profile)
 
 module.exports = routes;
